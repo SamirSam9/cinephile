6d48ba06e9d4908cafa06ef53a8a4473eee44daf
SaMiR <samir@examp.com>
2023-06-02 16:11:59 +0500

    asd

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <template>
-  <div>
+  <div class="wrapper">
+    <Navbar />
     <router-view />
   </div>
 </template>
 
 <script setup>
+import Navbar from "@/components/Navbar/Navbar.vue";
+
 
 </script>
 
