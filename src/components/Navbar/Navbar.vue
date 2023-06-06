716da4b772745d4ef4a6e8ea51bd2f6e08292ce6
SaMiR <samir@examp.com>
2023-06-06 16:24:18 +0500

    somr123

--- src/components/Navbar/Navbar.vue
+++ src/components/Navbar/Navbar.vue
@@ -6,8 +6,14 @@
                     <router-link to="/" class="logo">
                         <img src="@/assets/images/Logo.svg" alt="">
                     </router-link>
-                    <ul class="nav__list">
-                        <li v-for="link in links" :key="link.title">
+                    <button class="nav__btn" @click="burgerOn">
+                        <img src="@/assets/images/menu.svg" alt="">
+                    </button>
+                    <ul class="nav__list" :class="{active: burger}">
+                        <li class="nav__close" @click="burgrerOff">
+                            <img src="@/assets/images/closeMenu.svg" alt="">
+                        </li>
+                        <li v-for="link in links" :key="link.title" @click="burgerOff">
                             <router-link :to="link.url" class="nav__link">{{ link.title }}</router-link>
                         </li>
                         <li>
@@ -29,6 +35,19 @@ const links = ref([
     { title: 'Фильмы', url: '/movie' },
     { title: 'Сериалы', url: '/Tv' },
 ])
+
+const burger = ref(false)
+
+
+const burgerOn = ()=>{
+    burger.value = true
+    document.body.style.overflow = 'hidden'
+}
+const burgerOff = ()=>{
+    burger.value = false
+    document.body.style.overflow = 'visible'
+}
+
 
 </script>
 
