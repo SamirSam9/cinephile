7ddd651ca4e57df371aeb033ffdcabadfcce5145
SaMiR <samir@examp.com>
2023-06-09 16:55:06 +0500

    ssssooooommmmrrrrr

---
+++ src/components/UI/BtnMore.vue
@@ -0,0 +1,25 @@
+<template>
+    <router-link to="/" class="more">
+        <img src="@/assets/images/more.svg" alt="">
+        <span class="more__text"></span>
+    </router-link>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" >
+.more {
+    background: #149A03;
+    color: white;
+    display: flex;
+    align-items: center;
+    width: max-content;
+    border-radius: 10px;
+    &__text{
+    font-size: 20px;
+    font-weight: 400;
+    }
+}
+</style>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,6 +1,6 @@
 <template>
-    <div>
-        Home
+    <div class="main">
+       <Upcoming />
     </div>
 </template>
 
